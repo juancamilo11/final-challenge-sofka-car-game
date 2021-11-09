301e09517ed52df310d9533b9c48712311416f20
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-08 20:54:44 -0500

    Initial Setup of the project

---
+++ src/main/java/co/com/sofka/domain/generic/EventStoreRepository.java
@@ -0,0 +1,13 @@
+package co.com.sofka.domain.generic;
+
+import java.util.List;
+
+
+public interface EventStoreRepository {
+
+    List<DomainEvent> getEventsBy(String aggregateName, String aggregateRootId);
+
+
+    void saveEvent(String aggregateName, String aggregateRootId, StoredEvent storedEvent);
+
+}
